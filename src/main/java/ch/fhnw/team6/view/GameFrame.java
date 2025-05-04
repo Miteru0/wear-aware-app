db05e7d07b746904824ba5eb12d59ea61f4e016f
Miteru0 <miteruolen@gmail.com>
2025-05-04 20:29:13 +0200

    Clean the swing

--- src/main/java/ch/fhnw/team6/view/GameFrame.java
+++
@@ -1,9 +0,0 @@
-package ch.fhnw.team6.view;
-
-import javax.swing.JFrame;
-
-public class GameFrame extends JFrame {
-
-    
-
-}
--- src/test/java/ch/fhnw/team6/view/SwapBackgroundsTransitionTest.java
+++
@@ -1,4 +0,0 @@
-package ch.fhnw.team6.view;
-
-public class SwapBackgroundsTransitionTest {
-}
